67a55f67306cce7af4e927e4533a5d66e5ce3a46
Alexander Dikiy <vojtseh2014@gmail.com>
2023-10-07 12:58:29 +0300

    Explore websocket port

--- components/constants.ts
+++ components/constants.ts
@@ -1,7 +1,8 @@
 export const rpcAddresses: Map<string, string> =  new Map([
     ["osmo-test-5", "https://rpc.osmotest5.osmosis.zone"],
     ["juno-1", "https://rpc.uni.junonetwork.io"],
-    ["axelar-testnet-lisbon-3", "https://rpc-axelar-testnet.imperator.co"]
+    ["axelar-testnet-lisbon-3", "https://rpc-axelar-testnet.imperator.co"],
+    ["pion-1", "https://rpc.pion.rs-testnet.polypore.xyz/"]
 ]);
 
 export const apiUrl = "https://api.easypay.network";
