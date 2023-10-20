2ecd45a9b334456f5948fdbf99086c8e8ae0f65c
Alexander Dikiy <vojtseh2014@gmail.com>
2023-10-20 23:17:31 +0200

    Add comdex support

--- components/constants.ts
+++ components/constants.ts
@@ -3,11 +3,12 @@ export const rpcAddresses: Map<string, string> =  new Map([
     ["juno-1", "https://rpc.uni.junonetwork.io"],
     ["axelar-testnet-lisbon-3", "https://rpc-axelar-testnet.imperator.co"],
     ["pion-1", "https://rpc.pion.rs-testnet.polypore.xyz/"],
+    ["comdex-test3", "https://test3-rpc.comdex.one"],
     ["solana_testnet", "https://api.testnet.solana.com"],
     ["ethereum-sepolia-testnet", "https://sepolia.drpc.org"]
 ]);
 
-export const keplrNetworks = ["osmo-test-5", "juno-1", "axelar-testnet-lisbon-3", "pion-1"];
+export const keplrNetworks = ["osmo-test-5", "juno-1", "axelar-testnet-lisbon-3", "pion-1", "comdex-test3"];
 export const phantomNetworks = ["solana_testnet"];
 
 export const apiUrl = "https://api.easypay.network";
